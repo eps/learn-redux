f7012e67ec561076eccc69e03175797451042a3a
Herman Chan <hermchan89@gmail.com>
2018-01-22 17:27:27 -0800

    imported react router and initial router rendering

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Single extends React.Component {
+  render() {
+    return (
+      <div className="single-photo">
+        I'm the single 
+      </div>
+    );
+  }
+}
+
+export default Single;
