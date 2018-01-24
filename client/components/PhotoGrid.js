0155f52ea577ffeec6add45a386f8b69f15ddbb4
Herman Chan <hermchan89@gmail.com>
2018-01-23 16:49:45 -0800

    fixed state typo

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -4,10 +4,10 @@ import Photo from './Photo.js';
 
 class PhotoGrid extends React.Component {
   render() {
-    console.log(this.props.post);
+    console.log(this.props.posts);
     return (
       <div className="photo-grid">
-        {this.props.post.map((post, i) =>
+        {this.props.posts.map((post, i) =>
           <Photo {...this.props} key={i} i={i} post={post} />
         )}
       </div>
