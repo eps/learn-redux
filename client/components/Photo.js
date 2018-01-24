131239ae8c999825a600218ec77dc5a1586e928b
Herman Chan <hermchan89@gmail.com>
2018-01-23 16:23:42 -0800

    update state with actions using button onClick

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Photo extends React.Component {
+
   render() {
     const { post, i, comments } = this.props;
     return (
@@ -20,7 +21,9 @@ class Photo extends React.Component {
             <figcaption>
               <p>{post.caption}</p>
               <div className="control-buttons">
-                <button className="likes">&hearts; {post.likes}</button>
+                <button onClick={this.props.increment.bind(null, i)}
+                  className="likes">&hearts; {post.likes}
+                </button>
                 <Link className="button" to={`/view/${post.code}`}>
                   <span className="comment-count">
                     <span className="speech-bubble"></span>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,10 +3,30 @@
 // 1. an action (info about what happened)
 // 2. copy of current state
 
+// function posts(state = [], action) {
+//   console.log(state);
+//   switch(action.type) {
+//     case 'INCREMENT_LIKES' :
+//       return [
+//         ...state.slice(0, action.index),
+//         {...state[action.index], likes: state[action.index].likes + 1},
+//         ...state.slice(action.index + 1)
+//       ]
+//     default:
+//       return state;
+//   }
+// }
+// }
+//
 function posts(state = [], action) {
-  console.log('the post will change');
-  console.log(state, action);
-  return state;
-}
+  let newState = [...state];
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      newState[action.index].likes++;
+      return newState;
+    default:
+      return state;
+  }
+}﻿
 
 export default posts;
