c426a8dec81c3eb7ef35886aa1fadd67f11738a1
Herman Chan <hermchan89@gmail.com>
2018-01-24 18:17:34 -0800

    passing data to comments with bind

--- client/components/Comment.js
+++ client/components/Comment.js
@@ -6,21 +6,21 @@ class Comment extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
   renderComment(comment, i) {
     return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment"
+            onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>
+            &times;
+          </button>
         </p>
       </div>
     )
   }
-  componentDidMount() {
 
-  }
   handleSubmit(e){
     e.preventDefault();
     const { postId } = this.props.params;
@@ -31,10 +31,10 @@ class Comment extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    const { comment } = this.props;
     return (
       <div className="comments">
-        {this.props.comment.map(this.renderComment)}
+        {comment.map(this.renderComment.bind(this))}
         <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
